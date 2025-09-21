54ca9ae7647fce14c9dd8606f95ac62a68a0c506
IceLiuli <ocuex@outlook.com>
2025-09-21 14:07:14 +0800

    remake

--- old/config.js
+++ old/config.js
@@ -1,8 +1,7 @@
-// --- 配置 ---
-const data_file = '_data.txt'; // 指定数据文件路径
-const open_newtab = true; // true: 新标签页打开, false: 当前页跳转
+// 全局配置
+const data_file = './../_data.txt'; // 数据文件路径
 const case_sensitive = false; // 是否区分大小写
-let goafternote = false; // 控制是否在提示后跳转
+const open_newtab = true; // 是否在新标签页打开URL
 const enable_exceptional_case = true; // 否启用特殊情况处理
 
 // 特殊情况的正则表达式（可以在这里添加更多）
